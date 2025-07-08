d038f7d87415c084916075d894611b80814c9dce
Brad Grigsby <bgrigsby8@gmail.com>
2025-07-08 20:33:47 +0100

    Restored gripper press

--- gripper_press.go
+++ gripper_press.go
@@ -2,7 +2,7 @@ package viam_gripper_gpio
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"time"
 
 	"go.viam.com/rdk/components/board"
@@ -17,50 +17,23 @@ import (
 var GripperPressModel = family.WithModel("gripper-press")
 
 type ConfigPress struct {
-	Board    string
-	Pin      string // The pin to use for the gripper, if not using grab_pins or open_pins
-	Seconds  *int
-	GrabPins map[string]string `json:"grab_pins"`
-	OpenPins map[string]string `json:"open_pins"`
-	WaitPins map[string]string `json:"wait_pins,omitempty"`
-	OpenTime *int              `json:"open_time_ms,omitempty"`
-	GrabTime *int              `json:"grab_time_ms,omitempty"`
+	Board      string
+	Pin        string
+	Seconds    int
+	Geometries []spatialmath.GeometryConfig
 }
 
-func (cfg *ConfigPress) Validate(path string) ([]string, error) {
+func (cfg *ConfigPress) Validate(path string) ([]string, []string, error) {
 	if cfg.Board == "" {
-		return nil, utils.NewConfigValidationFieldRequiredError(path, "board")
+		return nil, nil, utils.NewConfigValidationFieldRequiredError(path, "board")
 	}
 
-	if cfg.Pin == "" && (cfg.GrabPins == nil || cfg.OpenPins == nil) {
-		return nil, utils.NewConfigValidationError(path, errors.New("either pin or grab_pins and open_pins must be specified"))
+	if cfg.Pin == "" {
+		return nil, nil, utils.NewConfigValidationFieldRequiredError(path, "pin")
 	}
 
-	if cfg.Pin != "" && (len(cfg.GrabPins) > 0 || len(cfg.OpenPins) > 0 || len(cfg.WaitPins) > 0) {
-		return nil, utils.NewConfigValidationError(path, errors.New("pin cannot be used with grab_pins, open_pins, or wait_pins"))
-	}
+	return []string{cfg.Board}, nil, nil
 
-	if cfg.Pin == "" && len(cfg.GrabPins) == 0 {
-		return nil, utils.NewConfigValidationError(path, errors.New("grab_pins must not be empty"))
-	}
-
-	if cfg.Pin == "" && len(cfg.OpenPins) == 0 {
-		return nil, utils.NewConfigValidationError(path, errors.New("open_pins must not be empty"))
-	}
-
-	for _, state := range cfg.GrabPins {
-		if state != "high" && state != "low" {
-			return nil, utils.NewConfigValidationError(path, errors.New("grab_pins must be 'high' or 'low'"))
-		}
-	}
-
-	for _, state := range cfg.OpenPins {
-		if state != "high" && state != "low" {
-			return nil, utils.NewConfigValidationError(path, errors.New("open_pins must be 'high' or 'low'"))
-		}
-	}
-
-	return []string{cfg.Board}, nil
 }
 
 func init() {
@@ -80,29 +53,28 @@ func newGripperPress(ctx context.Context, deps resource.Dependencies, config res
 
 	g := &myGripperPress{
 		name: config.ResourceName(),
-		mf:   referenceframe.NewSimpleModel(config.ResourceName().String()),
 		conf: newConf,
 	}
 
-	if g.conf.Seconds == nil {
-		defaultSeconds := 3
-		g.conf.Seconds = &defaultSeconds
+	if g.conf.Seconds <= 0 {
+		g.conf.Seconds = 3
 	}
 
-	defaultSecondsMs := 3000
-	if g.conf.GrabTime == nil {
-		g.conf.GrabTime = &defaultSecondsMs
+	b, err := board.FromDependencies(deps, newConf.Board)
+	if err != nil {
+		return nil, err
 	}
 
-	if g.conf.OpenTime == nil {
-		g.conf.OpenTime = &defaultSecondsMs
+	g.pin, err = b.GPIOPinByName(newConf.Pin)
+	if err != nil {
+		return nil, err
 	}
 
-	if g.conf.Pin != "" {
-		g.pins = map[string]string{g.conf.Pin: "high"}
+	g.geometries, err = ParseGeometries(newConf.Geometries)
+	if err != nil {
+		return nil, err
 	}
-
-	g.board, err = board.FromDependencies(deps, newConf.Board)
+	g.mf, err = gripper.MakeModel(g.name.ShortName(), g.geometries)
 	if err != nil {
 		return nil, err
 	}
@@ -116,10 +88,10 @@ type myGripperPress struct {
 	name resource.Name
 	mf   referenceframe.Model
 
-	conf *ConfigPress
+	conf       *ConfigPress
+	geometries []spatialmath.Geometry
 
-	pins  map[string]string
-	board board.Board
+	pin board.GPIOPin
 
 	open bool
 }
@@ -135,143 +107,25 @@ func (g *myGripperPress) Grab(ctx context.Context, extra map[string]interface{})
 	if !force(extra) && !g.open {
 		return false, nil
 	}
-
-	// If the "Pin" field is set, only use that pin to control grab/open
-	if len(g.pins) != 0 {
-		err := g.setPins(ctx, g.pins, true, extra)
-		if err != nil {
-			return false, err
-		}
-		g.open = false
-		// Return early if no grab time is specified
-		if *g.conf.Seconds == 0 {
-			return false, nil
-		}
-		time.Sleep(time.Second * time.Duration(*g.conf.Seconds))
-		err = g.setPins(ctx, g.pins, false, extra)
-		if err != nil {
-			return false, err
-		}
-		return false, nil
-	}
-
-	// Set any wait pins to their active state
-	if len(g.conf.WaitPins) > 0 {
-		err := g.setPins(ctx, g.conf.WaitPins, true, extra)
-		if err != nil {
-			return false, err
-		}
-	}
-
-	// Set the grab pins to their active state
-	err := g.setPins(ctx, g.conf.GrabPins, true, extra)
-	if err != nil {
-		return false, err
-	}
 	g.open = false
-
-	// Return early if no grab time is specified
-	if *g.conf.GrabTime == 0 || *g.conf.Seconds == 0 {
-		return false, nil
-	}
-	time.Sleep(time.Millisecond * time.Duration(*g.conf.GrabTime))
-
-	// Set the grab pins to their inactive state
-	err = g.setPins(ctx, g.conf.GrabPins, false, extra)
-	if err != nil {
-		return false, err
-	}
-
-	// Set any wait pins to their inactive state
-	if len(g.conf.WaitPins) > 0 {
-		err = g.setPins(ctx, g.conf.WaitPins, false, extra)
-		if err != nil {
-			return false, err
-		}
-	}
-
-	return false, nil
+	return false, g.press(ctx, extra)
 }
 
-func (g *myGripperPress) setPins(ctx context.Context, pins map[string]string, activate bool, extra map[string]interface{}) error {
-	for pinName, level := range pins {
-		pin, err := g.board.GPIOPinByName(pinName)
-		if err != nil {
-			return err
-		}
-		state := level == "high"
-		if !activate {
-			state = !state
-		}
-		err = pin.Set(ctx, state, extra)
-		if err != nil {
-			return err
-		}
+func (g *myGripperPress) press(ctx context.Context, extra map[string]interface{}) error {
+	err := g.pin.Set(ctx, true, extra)
+	if err != nil {
+		return err
 	}
-
-	return nil
+	time.Sleep(time.Second * time.Duration(g.conf.Seconds))
+	return g.pin.Set(ctx, false, extra)
 }
 
 func (g *myGripperPress) Open(ctx context.Context, extra map[string]interface{}) error {
 	if !force(extra) && g.open {
 		return nil
 	}
-
-	// If the "Pin" field is set, only use that pin to control grab/open
-	if len(g.pins) != 0 {
-		err := g.setPins(ctx, g.pins, true, extra)
-		if err != nil {
-			return err
-		}
-		g.open = true
-		// Return early if no grab time is specified
-		if *g.conf.Seconds == 0 {
-			return nil
-		}
-		time.Sleep(time.Second * time.Duration(*g.conf.Seconds))
-		err = g.setPins(ctx, g.pins, false, extra)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-
-	// Set any wait pins to their active state
-	if len(g.conf.WaitPins) > 0 {
-		err := g.setPins(ctx, g.conf.WaitPins, true, extra)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Set the open pins to their active state
-	err := g.setPins(ctx, g.conf.OpenPins, true, extra)
-	if err != nil {
-		return err
-	}
 	g.open = true
-
-	// Return early if no open time is specified
-	if *g.conf.OpenTime == 0 || *g.conf.Seconds == 0 {
-		return nil
-	}
-	time.Sleep(time.Millisecond * time.Duration(*g.conf.OpenTime))
-
-	// Set the open pins to their inactive state
-	err = g.setPins(ctx, g.conf.OpenPins, false, extra)
-	if err != nil {
-		return err
-	}
-
-	// Set any wait pins to their inactive state
-	if len(g.conf.WaitPins) > 0 {
-		err = g.setPins(ctx, g.conf.WaitPins, false, extra)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return g.press(ctx, extra)
 }
 
 func (g *myGripperPress) Name() resource.Name {
@@ -295,9 +149,25 @@ func (g *myGripperPress) Stop(context.Context, map[string]interface{}) error {
 }
 
 func (g *myGripperPress) Geometries(context.Context, map[string]interface{}) ([]spatialmath.Geometry, error) {
-	return []spatialmath.Geometry{}, nil
+	return g.geometries, nil
 }
 
 func (g *myGripperPress) ModelFrame() referenceframe.Model {
 	return g.mf
+}
+
+func (g *myGripperPress) CurrentInputs(ctx context.Context) ([]referenceframe.Input, error) {
+	return []referenceframe.Input{}, nil
+}
+
+func (g *myGripperPress) GoToInputs(ctx context.Context, inputs ...[]referenceframe.Input) error {
+	return fmt.Errorf("GoToInputs not implemented")
+}
+
+func (g *myGripperPress) IsHoldingSomething(ctx context.Context, extra map[string]interface{}) (gripper.HoldingStatus, error) {
+	return gripper.HoldingStatus{}, fmt.Errorf("IsHoldingSomething not implemented")
+}
+
+func (g *myGripperPress) Kinematics(ctx context.Context) (referenceframe.Model, error) {
+	return g.mf, nil
 }
